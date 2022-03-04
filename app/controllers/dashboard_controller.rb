353147aaf6d0a34cd68d586305eb82c27b32a128
Nicolás Cárcamo <falconrandom@gmail.com>
2022-03-03 21:39:28 -0300

    implementando graficos

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class DashboardController < ApplicationController
   def index
+    @sales_count_last_year = Sale.group_by_month(:date, range: 12.months.ago.midnight..Time.now).order(date: :asc).sum(:price)
+
+    @sales_quantity_last_year = Sale.group_by_month(:date, range: 12.months.ago.midnight..Time.now).order(date: :asc).count
+
+    @sales_average_last_year = Sale.group_by_month(:date, range: 12.months.ago.midnight..Time.now).order(date: :asc).sum(:price)
+
+    @sales_by_origin = Sale.group(:origin).sum(:price)
+
+    @sales_by_blend = Sale.group(:name).sum(:price)
   end
 end
